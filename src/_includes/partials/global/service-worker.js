c667e0f754400159bb4c9423c4fa4f8acc9e676e
Jonny <jonnyburch@gmail.com>
2019-07-04 23:25:54 +0100

    still can't get fonts working

--- src/_includes/partials/global/service-worker.js
+++ src/_includes/partials/global/service-worker.js
@@ -12,7 +12,7 @@ const EXCLUDED_URLS = [
 ];
 
 // URLS that we want to be cached when the worker is installed
-const PRE_CACHE_URLS = ['/', '/fonts/lora-v13-latin-700.woff2'];
+const PRE_CACHE_URLS = ['/', '/fonts/basiercircle-regular-webfont.woff2'];
 
 // You might want to bypass a certain host
 const IGNORED_HOSTS = ['localhost', 'unpkg.com', ];
