349600e2fce0d0589a606d9157eec1d8a3e77815
Yuya Saito <studiomohawk@gmail.com>
2020-02-01 13:25:40 +0900

    feat: Use node-jq

--- _task/node-opml-to-blogroll.js
+++ _task/node-opml-to-blogroll.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const parser = require('fast-xml-parser');
+const jq = require('node-jq');
 
 const OPML_PATH = path.resolve(process.env.PWD, '_task');
 const DATA_PATH = path.resolve(process.env.PWD, 'src/_data');
@@ -8,37 +9,49 @@ const DATA_PATH = path.resolve(process.env.PWD, 'src/_data');
 const opml = fs.readFileSync(`${OPML_PATH}/subscriptions.xml`, 'utf-8');
 
 // Helpers function
-const removeDuplicates = (array, key) => {
-  let lookup = new Set();
-  return array.filter(obj => !lookup.has(obj[key]) && lookup.add(obj[key]));
+const transformJSON = async json => {
+  const filter = '.opml | {dateCreated: .head.dateCreated, items: .body.outline.outline} | .items|=map(del(._text, ._type)) | .items|=unique_by(._xmlUrl) | .items|=sort_by(._title)';
+
+  const option = {
+    input: 'string'
+  };
+
+  try {
+    transformed = await jq.run(filter, json, option);
+
+    return transformed;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-try {
-  const options = {
-    attributeNamePrefix: "_",
-    attrNodeName: false,
-    textNodeName : "#text",
-    ignoreAttributes: false,
-    ignoreNameSpace: false,
-    allowBooleanAttributes: true,
-    parseNodeValue : true,
-    parseAttributeValue : false,
-    trimValues: true,
-    arrayMode: false
-  };
+// Main Function
+async function main() {
+  try {
+    const options = {
+      attributeNamePrefix: "_",
+      attrNodeName: false,
+      textNodeName : "#text",
+      ignoreAttributes: false,
+      ignoreNameSpace: false,
+      allowBooleanAttributes: true,
+      parseNodeValue : true,
+      parseAttributeValue : false,
+      trimValues: true,
+      arrayMode: false
+    };
 
-  const parsed = parser.parse(opml, options, true);
-  const uniqueObj = removeDuplicates(parsed.opml.body.outline.outline, '_xmlUrl');
+    const parsed = parser.parse(opml, options, true);
+    const json = JSON.stringify(parsed, null, 2);
+    const blogroll = await transformJSON(json);
 
-  const finalObj = {
-    dateCreated: parsed.opml.head.dateCreated,
-    items: uniqueObj
-  };
-  const blogroll = JSON.stringify(finalObj, null, 2);
 
-  console.log(blogroll);
+    console.log(blogroll);
 
-  fs.writeFileSync(`${DATA_PATH}/opml.json`, blogroll, 'utf-8');
-} catch (error) {
-  console.log(error.message)
+    fs.writeFileSync(`${DATA_PATH}/opml.json`, blogroll, 'utf-8');
+  } catch (error) {
+    console.log(error.message)
+  }
 }
+
+main();
