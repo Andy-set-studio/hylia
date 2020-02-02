2a07f6bb2b8fa5885b78587361103fb2d9fe885b
Yuya Saito <studiomohawk@gmail.com>
2020-02-02 17:24:04 +0900

    feat: Use feedbin api to generate blogroll

--- _task/node-opml-to-blogroll.js
+++ _task/node-opml-to-blogroll.js
@@ -1,16 +1,51 @@
 const path = require('path');
 const fs = require('fs');
-const parser = require('fast-xml-parser');
-const jq = require('node-jq');
 
-const OPML_PATH = path.resolve(process.env.PWD, '_task');
+const jq = require('node-jq');
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+
 const DATA_PATH = path.resolve(process.env.PWD, 'src/_data');
 
-const opml = fs.readFileSync(`${OPML_PATH}/subscriptions.xml`, 'utf-8');
+dotenv.config();
+
+// Configuration
+/// Feedbin API URL
+const FEEDBIN_API_URL = 'https://api.feedbin.com/v2/subscriptions.json';
+
+/// Configure Feedbin API option
+const FEEDBIN_API_OPTION = {
+  'method': "GET",
+  'headers': {
+    'Authorization': `Basic ${ process.env.FEEDBIN_API_TOKEN}`
+  }
+};
 
 // Helpers function
+/// Function to return unknown errors
+const handleError = err => {
+  console.error(err);
+  process.exit(1);
+};
+
+/// Fetch latest subscriptions from Feedbin
+const getSubscription = async () => {
+  try {
+    const response = await fetch(
+      `${FEEDBIN_API_URL}`,
+      FEEDBIN_API_OPTION
+    );
+
+    return await response.json();
+
+  } catch (err) {
+    handleError(err);
+  }
+};
+
+/// Function to transform JSON using node-jq
 const transformJSON = async json => {
-  const filter = '.opml | {dateCreated: .head.dateCreated, items: .body.outline.outline} | .items|=map(del(._text, ._type)) | .items|=unique_by(._xmlUrl) | .items|=sort_by(._title)';
+  const filter = '{dateCreated: .[0].created_at, items: [.[]]} | .items|=map(del(.id, .feed_id)) | .items|=unique_by(.feed_url) | .items|=sort_by(.title)';
 
   const option = {
     input: 'string'
@@ -25,31 +60,13 @@ const transformJSON = async json => {
 
 // Main Function
 async function main() {
-  try {
-    const options = {
-      attributeNamePrefix: "_",
-      attrNodeName: false,
-      textNodeName : "#text",
-      ignoreAttributes: false,
-      ignoreNameSpace: false,
-      allowBooleanAttributes: true,
-      parseNodeValue : true,
-      parseAttributeValue : false,
-      trimValues: true,
-      arrayMode: false
-    };
-
-    const parsed = parser.parse(opml, options, true);
-
-    const json = JSON.stringify(parsed, null, 2);
+    const subscription = await getSubscription();
+    const json = JSON.stringify(subscription, null, 2);
     const blogroll = await transformJSON(json);
 
     console.log(blogroll);
 
     fs.writeFileSync(`${DATA_PATH}/opml.json`, blogroll, 'utf-8');
-  } catch (error) {
-    console.log(error.message)
-  }
 }
 
 main();
