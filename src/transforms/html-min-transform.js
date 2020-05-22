83d8a07fb3639c7ded6bca6a4a4163e359fa38f7
Johann Schopplich <mail@johannschopplich.com>
2020-05-22 11:56:37 +0200

    Simplify html minify transform config

--- src/transforms/html-min-transform.js
+++ src/transforms/html-min-transform.js
@@ -1,14 +1,12 @@
 const htmlmin = require('html-minifier');
 
 module.exports = function htmlMinTransform(value, outputPath) {
-  if (outputPath.indexOf('.html') > -1) {
-    let minified = htmlmin.minify(value, {
-      useShortDoctype: true,
-      removeComments: true,
-      collapseWhitespace: true,
-      minifyCSS: true
-    });
-    return minified;
-  }
-  return value;
+  if (!outputPath.endsWith('.html')) return value;
+
+  return htmlmin.minify(value, {
+    useShortDoctype: true,
+    removeComments: true,
+    collapseWhitespace: true,
+    minifyCSS: true
+  });
 };
